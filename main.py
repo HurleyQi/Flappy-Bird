9c80d3f2aca2332ceab853334b2ec4c565dd7d06
Hurley Qi <qikangsheng@gmail.com>
2023-09-09 23:32:57 -0500

    adding comments

--- main.py
+++ main.py
@@ -4,11 +4,14 @@ from pygame.locals import *
 import random
 import time
 
+# possible heights
 height=[250,300,400,500,550,600,650]
 
 SIZE = 5
 
+# class the represents the pipes in the game
 class Pipes:
+    # initialization
     def __init__(self, parent_screen, length):
         self.length = length
         self.parent_screen = parent_screen
@@ -17,6 +20,7 @@ class Pipes:
         self.x = []
         self.y = []
         self.direction = "right"
+    # drawing and displaying pipes on screen
     def draw(self):
         self.parent_screen.fill((110,110,5))
         for i in range(self.length):
@@ -40,7 +44,9 @@ class Pipes:
 
         self.draw()
 
+# class the represents the bird in the game
 class Bird:
+    # initialization
     def __init__(self,parent_screen):
         self.parent_screen = parent_screen
         self.bird = pygame.image.load("./bird.png")
@@ -48,6 +54,7 @@ class Bird:
         self.direction = "down"
         self.x = 200
         self.y = 0
+    # drawing and displaying bird on screen
     def draw(self):
         self.parent_screen.blit(self.image,(self.x,self.y))
         pygame.display.flip()
@@ -56,8 +63,9 @@ class Bird:
             self.y += 7
         self.draw()
 
-
+# game 
 class Game:
+    # initialization
     def __init__(self):
         pygame.init()
         self.surface = pygame.display.set_mode((1500,800))
@@ -67,6 +75,7 @@ class Game:
         self.bird = Bird(self.surface)
         self.bird.draw()
     def if_collision(self):
+        # conditions for when to quit
         for i in range(self.pipes.length):
             if self.bird.y >= self.pipes.y[i] and self.pipes.x[i] == 350:
                 exit(0)
@@ -76,10 +85,12 @@ class Game:
                 exit(0)
             # if self.bird.y <= (self.pipes.y[i] - 80) and self.pipes.x[i] == 350:
             #     exit(0)
+    # actual running the game
     def run(self):
         running = True
         while running:
             for event in pygame.event.get():
+                # commands and their responses
                 if event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
                         running = False
